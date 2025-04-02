d5e2c5349b2db2d55c00d24940b3086b458276da
PipzhasP <jorgeeduardorocillo@gmail.com>
2025-04-01 21:00:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-window.location.href ="metal.html"
+// Selección de los enlaces
+const pizzeriaLink = document.querySelector('a[href="pizzeria.html"]');
+const proyectoLink = document.querySelector('a[href="metal.html"]');
+
+// Evento para redirigir o realizar una acción cuando se hace clic
+pizzeriaLink.addEventListener('click', () => {
+    console.log("Se hizo clic en el enlace a la Pizzería");
+});
+
+proyectoLink.addEventListener('click', () => {
+    console.log("Se hizo clic en el enlace al Proyecto Integral");
+});
